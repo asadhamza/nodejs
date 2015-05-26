4e020aafa39a2defc04b75071d2b5dee184adce2
Asad <asadhamza@live.com>
2015-05-26 17:05:27 +0500

    Edit First

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Node Demo App' });
+});
+router.post('/',function(req,res){
+
+  res.send(req.body.name);
 });
 
 module.exports = router;
